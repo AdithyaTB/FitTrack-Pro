a2d4bb2a7dc9f2a58885432b3a821471eb1f0cf5
Adithya T B <adithya.t.b.24@gmail.com>
2026-02-04 13:14:02 +0530

    API Updation

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const API = axios.create({
-    baseURL: import.meta.env.VITE_API_URL,
+    baseURL: import.meta.env.VITE_API_URL || "https://fit-track-pro-backend-adhi.onrender.com/api",
 });
 
 API.interceptors.request.use((req) => {
